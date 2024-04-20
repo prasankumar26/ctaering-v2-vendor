449b06578a294bb604989807a34c93d4768982f5
Prasan Kumar <90718794+prasankumar26@users.noreply.github.com>
2024-04-20 17:25:28 +0530

    business profile

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,9 +1,50 @@
 import { Outlet } from "react-router-dom";
 import Grid from '@mui/material/Grid';
 import LeftNav from "../components/nav/LeftNav";
-import TopHeader from "../components/global/TopHeader";
+import { useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { api, BASE_URL } from "../api/apiConfig";
+
 
 const Layout = () => {
+  const { accessToken, refreshToken } = useSelector((state) => state.user.accessToken);
+
+
+  // useEffect(() => {
+  //   const checkTokenExpiration = async () => {
+  //     const accessTokenExp = getExpirationTime(accessToken);
+  //     const currentTimestamp = Math.floor(Date.now() / 1000);
+
+  //     if (accessTokenExp && accessTokenExp < currentTimestamp) {
+  //       try {
+  //         const response = await api.post(`${BASE_URL}/refresh-token`, { refreshToken, accessToken }, {
+  //           headers: {
+  //             Authorization: `${refreshToken} ${accessToken}`
+  //           }
+  //         })
+  //         console.log(response, "response");
+  //         const { accessToken: newAccessToken } = response.data;
+  //         console.log(newAccessToken, "newAccessToken");
+
+  //       } catch (error) {
+  //         console.error("Error refreshing token:", error);
+  //       }
+  //     }
+  //   }
+
+  //   checkTokenExpiration()
+  // }, [accessToken, refreshToken])
+
+  // const getExpirationTime = (token) => {
+  //   if (!token) return null;
+  //   const decodedToken = jwtDecode(token);
+  //   return decodedToken.exp;
+  // }
+
+
+
   return (
     <>
       <div className="dashboard-container">
