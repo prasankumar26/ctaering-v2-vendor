ee9a05b745b913a5be639ccfc6f5ae19adad650e
Prasan Kumar <90718794+prasankumar26@users.noreply.github.com>
2024-05-04 20:55:22 +0530

    raise a ticket

--- src/pages/RaiseTicket.jsx
+++ src/pages/RaiseTicket.jsx
@@ -10,10 +10,23 @@ import { useState } from "react";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useNavigate } from 'react-router-dom';
 import Box from '@mui/material/Box';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { setIsLoading } from "../features/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { api, BASE_URL } from "../api/apiConfig";
+import toast from "react-hot-toast";
+import { datavalidationerror, successToast } from "../utils";
 
+const initialState = {
+    issue: '',
+    comments: '',
+}
 
 const RaiseTicket = () => {
 
+    const { accessToken, isLoading } = useSelector((state) => state.user);
+    const dispatch = useDispatch()
     const navigate = useNavigate();
 
     const handleBack = () => {
@@ -47,6 +60,37 @@ const RaiseTicket = () => {
     }));
 
 
+    const schema = Yup.object().shape({
+        issue: Yup.string().required('Issue is required.'),
+        comments: Yup.string().required('Comments is required.')
+    });
+
+
+    const handleSubmit = async (values, resetForm) => {
+        const { issue, comments } = values;
+        const data = {
+            issue,
+            comments,
+        }
+
+        dispatch(setIsLoading(true))
+        try {
+            const response = await api.post(`${BASE_URL}/vendor-raise-ticket`, data, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            })
+            toast.success(successToast(response))
+            resetForm(initialState);
+            dispatch(setIsLoading(false))
+        } catch (error) {
+            console.log(error);
+            toast.error(datavalidationerror(error))
+        }
+
+    }
+
+
 
     return (
         <>
@@ -60,58 +104,65 @@ const RaiseTicket = () => {
                         <Grid item xs={12} sm={6} md={6} lg={6}>
                             <div className="ct-box ct-box-padding">
                                 <div className="px-4">
-
                                     <Stack direction="row" alignItems="center" className="mb-3">
                                         <ArrowBackIcon className="cursor-pointer" style={{ fontSize: '18px', color: '#c33332' }} onClick={handleBack} /> <h3 className="faq-heading ms-2">Help Desk / Support</h3>
                                     </Stack>
-
                                     <h2 className="rt-heading mb-4 mt-5">Raise a Ticket</h2>
+                                    <Formik enableReinitialize={true} initialValues={initialState} validationSchema={schema} onSubmit={(values, { resetForm }) => handleSubmit(values, resetForm)}>
+                                        {({ values, errors, handleChange, handleSubmit }) => (
+                                            <form onSubmit={handleSubmit} className="px-4">
 
-                                    <CssTextField
-                                        id="outlined-number"
-                                        variant="outlined"
-                                        label="Issue"
-                                        className='mb-3'
-                                        style={{ width: '100%' }}
-                                        InputLabelProps={{
-                                            style: { color: '#777777', fontSize: '10px' },
-                                        }}
-                                        InputProps={{
-                                            style: {
-                                                borderRadius: '8px',
-                                                backgroundColor: '#FFFFFF',
-                                            }
-                                        }}
-                                    />
-
-
-                                    <div className="mt-1">
-                                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                                            <textarea
-                                                rows="20"
-                                                name="comment[text]"
-                                                id="comment_text"
-                                                cols="40"
-                                                placeholder="comments..."
-                                                className="job-textarea"
-                                                autoComplete="off"
-                                                role="textbox"
-                                                aria-autocomplete="list"
-                                                aria-haspopup="true"
-                                            ></textarea>
-                                        </Box>
-                                    </div>
+                                                <CssTextField
+                                                    id="outlined-number"
+                                                    variant="outlined"
+                                                    label="Issue"
+                                                    className='mb-1'
+                                                    value={values.issue}
+                                                    onChange={handleChange}
+                                                    name="issue"
+                                                    style={{ width: '100%' }}
+                                                    InputLabelProps={{
+                                                        style: { color: '#777777', fontSize: '10px' },
+                                                    }}
+                                                    InputProps={{
+                                                        style: {
+                                                            borderRadius: '8px',
+                                                            backgroundColor: '#FFFFFF',
+                                                        }
+                                                    }}
+                                                />
+                                                {errors.issue && <small className='text-danger mt-0 ms-1'>{errors.issue}</small>}
 
 
+                                                <div className="mt-3">
+                                                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                                        <textarea
+                                                            value={values.comments}
+                                                            onChange={handleChange}
+                                                            name="comments"
+                                                            rows="20"
+                                                            id="comment_text"
+                                                            cols="40"
+                                                            placeholder="comments..."
+                                                            className="job-textarea"
+                                                            autoComplete="off"
+                                                            role="textbox"
+                                                            aria-autocomplete="list"
+                                                            aria-haspopup="true"
+                                                        ></textarea>
+                                                    </Box>
+                                                    {errors.comments && <small className='text-danger mt-2 ms-1'>{errors.comments}</small>}
 
-                                    <Stack direction="row" justifyContent="center" className="mt-4">
-                                        <Button variant="contained" className="inquiries-red-btn"> Submit </Button>
-                                    </Stack>
+                                                </div>
 
-
-
-
-
+                                                <Stack direction="row" justifyContent="center" className="mt-4">
+                                                    <Button variant="contained" className="inquiries-red-btn" type="submit" disabled={isLoading}>
+                                                        {isLoading ? 'Loading...' : 'Submit'}
+                                                    </Button>
+                                                </Stack>
+                                            </form>
+                                        )}
+                                    </Formik>
                                 </div>
                             </div>
                         </Grid>
