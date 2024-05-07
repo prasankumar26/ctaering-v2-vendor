f57e4bd48ee8a2fd699838bddb670d1637d07128
Prasan Kumar <90718794+prasankumar26@users.noreply.github.com>
2024-05-07 20:05:48 +0530

    layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,41 +1,26 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import Grid from '@mui/material/Grid';
 import LeftNav from "../components/nav/LeftNav";
+import { useSelector } from "react-redux";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { api, BASE_URL } from "../api/apiConfig";
-import { setAccessToken, setRefreshToken } from "../features/user/userSlice";
-import toast from "react-hot-toast";
-import { datavalidationerror, successToast } from "../utils";
 import { jwtDecode } from "jwt-decode";
-import { useTokenInterceptor } from "../hooks/useTokenInterceptor";
 
 const Layout = () => {
-  const { accessToken, refreshToken } = useSelector((state) => state.user);
+  const { accessToken } = useSelector((state) => state.user);
   const navigate = useNavigate();
-  const dispatch = useDispatch();
 
-console.log(refreshToken,"refreshToken");
-  useTokenInterceptor();
-
-
-
-  const test = async () => {
-    try {
-      const refreshResponse = await api.post(`${BASE_URL}/token-refresh`, {
-        headers: {
-          Authorization: `Bearer ${refreshToken}`,
-        }
-      });
-      console.log(refreshResponse, "refreshResponse");
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   useEffect(() => {
-    test()
-  }, [])
+    if (accessToken) {
+      const decodedToken = jwtDecode(accessToken);
+      const currentTime = Date.now() / 1000;
+      console.log(decodedToken.exp < currentTime, "decodedToken.exp < currentTime");
+      if (decodedToken.exp < currentTime) {
+        navigate("/create-account");
+      }
+    }
+  }, [accessToken, navigate]);
+
 
   return (
     <>
